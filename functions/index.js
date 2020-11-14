f18588255002543e7067e3c107f805f782ec7409
miroslav.rac <miroslav.rac@gmail.com>
2020-11-14 14:12:23 +0100

    added dummy route

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express');
+
+// Init app
+const app = express();
+
+// Automatically allow cross-origin requests
+const cors = require('cors');
+app.use(cors({ origin: true }));
+
+
+app.get('/:id', (req, res) => {
+
+  res.send({
+    status: 'works'
+  })
+
+});
+
+exports.route = functions.https.onRequest(app);
